b8e8f1bb35657cc3d7c77a86bca1d69abca777fa
Maghawry <mag.hauwry@gmail.com>
2022-03-10 22:29:33 +0100

    WorkAndEarn function has been added

--- app.js
+++ app.js
@@ -1,3 +1,65 @@
 // getElements
+const barrowButton = document.getElementById("barrow")
+const incomeLabel = document.getElementById("earned")
+const balanceLabel = document.getElementById("balance")
+const debtLabel = document.getElementById("debt")
 
-barrowMoney()
+const products = [];
+let income = 0.0;
+let balance = income; // is this right?
+let debt = 0;
+
+//default
+incomeLabel.innerText = "€ 0";
+balanceLabel.innerText = "€ 0";
+
+
+//Income area
+function WorkandEarn() {
+    let hoursWorked = prompt("How many hours have you worked?");
+    income += parseInt(hoursWorked)*20;
+    incomeLabel.innerText = `€ ${income}`;
+}
+
+function transferEarningsToBank() {
+    
+    
+    if (!debt === 0){
+        
+
+    } else {
+        balance += income;
+        balanceLabel.innerText = `€ ${balance}`;
+    }
+
+    income = 0.0;
+    incomeLabel.innerText = `€ ${income}`;
+
+    if (debt === 0){
+        debtLabel.innerText = "";
+    }
+}
+
+function barrowMoney() {
+
+    if (parseInt(debt) === 0){
+
+        debt = prompt(`
+    How much money would you like to barrow?
+    --------------------------------------------------
+    note: You cannot barrow double your balance.
+    `)
+    if (debt <= balance*2-1){
+        debtLabel.innerText = `Debt: € ${debt}`;
+        balance += parseInt(debt);
+        balanceLabel.innerText = `€ ${balance}`;
+    }
+    }else {
+        alert("You previous loan has not been payed off.")
+    }
+    
+    
+
+    balanceLabel.innerText = `€ ${balance}`;
+    incomeLabel.innerText = `€ ${income}`;
+}
