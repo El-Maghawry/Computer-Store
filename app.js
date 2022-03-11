884bce80db6f96b2a766b542c60082da2aa0a9ea
Maghawry <mag.hauwry@gmail.com>
2022-03-11 16:02:33 +0100

    Payback loan from income/salary functionality added.

--- app.js
+++ app.js
@@ -1,44 +1,48 @@
 // getElements
 const barrowButton = document.getElementById("barrow")
+const paybackButton = document.getElementById("payback")
 const incomeLabel = document.getElementById("earned")
 const balanceLabel = document.getElementById("balance")
 const debtLabel = document.getElementById("debt")
 
 const products = [];
-let income = 0;
-let balance = income; // is this right?
+let income = 0.0;
+let balance = 0.0; 
 let debt = 0.0;
+
 
 //default
 incomeLabel.innerText = "€ 0";
 balanceLabel.innerText = "€ 0";
+paybackButton.style.visibility = "hidden";
 
 
 //Income area
 function WorkandEarn() {
-    let hoursWorked = prompt("How many hours have you worked?");
-    income += parseInt(hoursWorked)*20;
+    let hoursWorked = prompt(`
+    How many hours have you worked?
+    • You earn € 100 for every hour. 
+    `);
+    income += parseInt(hoursWorked)*100;
     incomeLabel.innerText = `€ ${income}`;
 }
 
 function transferEarningsToBank() {
-    dept = parseFloat(debt)
-    console.log(income*0.1, parseFloat(debt), typeof parseFloat(debt))
-    
-    if (parseFloat(debt) !== 0.0){
-        console.log("debt is not 0.0")
-        if(income*0.1 <= parseFloat(debt)){
+
+    if (debt !== 0.0 && debt !== null && debt !== 0 && !isNaN(debt)){
+
+        if(income*0.1 <= debt){
             debt -= income*0.1
             balance += income*0.9
             balanceLabel.innerText = `€ ${balance}`;
             debtLabel.innerText = `Debt: € ${debt}`;
             console.log(`You still have a debt of € ${debt}`)
         }else {
-            income -= parseFloat(debt);
+            income -= debt;
             debt = 0.0;
             balance += income;
             balanceLabel.innerText = `€ ${balance}`;
-            debtLabel.innerText = `Debt: € ${debt}`;
+            debtLabel.innerText = "";
             console.log("debt has been payed off")
 
         }
@@ -51,32 +55,67 @@ function transferEarningsToBank() {
 
     income = 0.0;
     incomeLabel.innerText = `€ ${income}`;
-
-    if (debt === 0.0){
-        debtLabel.innerText = "";
-    }
 }
 
+// Bank Area
 function barrowMoney() {
+    console.log(debt)
 
-    if (debt === 0.0){
+    if (debt === 0.0 || debt === null || debt === 0 || isNaN(debt)){
 
-        debt = prompt(`
+        debt = parseFloat(prompt(`
     How much money would you like to barrow?
     --------------------------------------------------
-    note: You cannot barrow double your balance.
-    `)
-    if (debt <= balance*2-1){
+    • You cannot barrow more than double your balance.
+    • Every money transfer takes 20% to pay off your loan.
+    • You will not rent over the money owed. 
+    `))
+    if (debt < balance*2+1 && debt !== null && debt !== 0 && !isNaN(debt)){
+        balance += parseFloat(debt);
         debtLabel.innerText = `Debt: € ${debt}`;
-        balance += parseInt(debt);
-        balanceLabel.innerText = `€ ${balance}`;
-    }
-    }else {
-        alert("You previous loan has not been payed off.")
-    }
-    
-    
+        balanceLabel.innerText = `€ ${balance-1}`;
+        paybackButton.style.visibility = "visible";
 
+    } else if (debt === 0.0 || debt === null || debt === 0 || debt === NaN){
+        debtLabel.innerText = "";
+        alert("Canceled")
+        debt === 0.0
+        paybackButton.style.visibility = "hidden";
+ 
+    }
+    console.log(debt)
     balanceLabel.innerText = `€ ${balance}`;
     incomeLabel.innerText = `€ ${income}`;
+
+}else { 
+    alert("You previous loan has not been payed off.")
 }
+}
+
+function paybackMoney(){
+    console.log(typeof debt)
+
+        if (income >= debt){
+            income -= debt;
+            balance += income
+            debt = 0.0
+            income = 0.0
+            debtLabel.innerText = "";
+            paybackButton.style.visibility = "hidden";
+  
+        }else {
+            debt -= income;
+            income = 0.0
+            debtLabel.innerText = `Debt: € ${debt}`;
+
+        }
+        
+        balanceLabel.innerText = `€ ${balance}`;
+        incomeLabel.innerText = `€ ${income}`;
+
+    }
+
+    
+
+
+
